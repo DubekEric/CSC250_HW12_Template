3e0b992c92310a75a21f7c4f93a07ff2056702a4
awesomefat <awesomefat@gmail.com>
2017-11-09 10:42:14 -0600

    solution to HW 12

--- app/src/main/java/com/example/awesomefat/csc250_hw12_template/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc250_hw12_template/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,15 +18,29 @@ public class MainActivity extends AppCompatActivity
         this.setContentView(R.layout.activity_main);
     }
 
+    private String reverse(String s)
+    {
+        String answer = "";
+        for(int i = s.length()-1; i >= 0; i--)
+        {
+            answer = answer + s.charAt(i);
+        }
+        return answer;
+    }
+
     //MVC expose this function to the interface builder
     public void onChangeButtonPressed(View v)
     {
+        //we are in an instance CONTEXT
         //v will be the widget that calls this function upon being clicked
 
         //MVC - linking interface widget to local variable
         EditText inputET = (EditText)this.findViewById(R.id.inputET);
+        EditText inputET2 = (EditText)this.findViewById(R.id.inputET2);
         TextView myLabel = (TextView)this.findViewById(R.id.myLabel);
-        myLabel.setText(inputET.getText().toString());
+        String reverse = this.reverse(inputET.getText().toString());
+        String reverse2 = this.reverse(inputET2.getText().toString());
+        myLabel.setText(reverse + reverse2);
     }
 
 }
